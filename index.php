755c5280e2b79de236f820ee652137d547b4bd3f
jacob bapple <jake.bapple@gmail.com>
2019-09-19 10:56:16 -0600

    testing please

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
-<html>
-	<header>
-		<title>Why World</title>
-	</header>
+<?php $thisPage="Home"; ?>
+<html lang="en">
+	<head>
+		<title>Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?></title>
+		<meta name="title" content="Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
+		<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?>" />
+	  <meta charset="utf-8">
+
+	  <link rel="stylesheet" href="css/styles.css">
+
+	</head>
 
 	<body>
-		<?php 
-			include("phpincludes/navigation.php"); 
-		?>
-		
-	  <h1>Why World, Why</h1>
+		<?php include("phpincludes/navigation.php"); ?>
+	  <h1>Hello Woild</h1>
 	</body>
+
 </html>
--- phpincludes/navigation.php
+++ phpincludes/navigation.php
@@ -1,11 +1,11 @@
 <?php
 	<nav>
 		<ul>
-			<li><a href="">Home</a></li>
-			<li><a href="">About</a></li>
-			<li><a href="">Games</a></li>
-			<li><a href="">Go Premium</a></li>
-			<li><a href="">Contact</a></li>
+			<li<?php if ($thisPage=="Home") echo " class=\"active\""; ?>><a href="#">Home</a></li>
+			<li<?php if ($thisPage=="About") echo " class=\"active\""; ?>><a href="#">About</a></li>
+			<li<?php if ($thisPage=="Games") echo " class=\"active\""; ?>><a href="#">Games</a></li>
+			<li<?php if ($thisPage=="Go Premium") echo " class=\"active\""; ?>><a href="#">Go Premium</a></li>
+			<li<?php if ($thisPage=="Contact") echo " class=\"active\""; ?>><a href="#">Contact</a></li>
 		</ul>
 	</nav>
 ?>
