495712de30641f8b6d55a80ed96bd5f116b7046c
jacob bapple <jake.bapple@gmail.com>
2019-09-18 18:58:03 -0600

    testing includes

---
+++ phpincludes/navigation.php
@@ -0,0 +1,11 @@
+<?php
+	<nav>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">About</a></li>
+			<li><a href="">Games</a></li>
+			<li><a href="">Go Premium</a></li>
+			<li><a href="">Contact</a></li>
+		</ul>
+	</nav>
+?>
