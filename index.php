94e85670dfcea922ef1b217d3e64fcdd6e45de06
jacob bapple <jake.bapple@gmail.com>
2019-09-19 11:13:40 -0600

    garble

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php $thisPage="Home"; ?>
 <head>
 	<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
-	<meta name="title" content="Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
+	<meta name="title" content="Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?>" />
 	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
   <meta charset="utf-8">
 
@@ -10,6 +10,7 @@
 </head>
 
 <body>
-	<?php require "/phpincludes/navigation.php"; ?>
+	<h1>How about now</h1>
+	<?php require_once "phpincludes/navigation.php"; ?>
   <h1>Hello Woild</h1>
 </body>
