b7bf1aebab3652ceb861132a7554b20b34a08850
jacob bapple <jake.bapple@gmail.com>
2019-09-19 11:07:58 -0600

    restarting

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php $thisPage="Home"; ?>
-<head>
-	<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
-	<meta name="title" content="Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
-	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
-  <meta charset="utf-8">
-
-  <link rel="stylesheet" href="css/styles.css">
-
-</head>
-
-<body>
-	<?php require "/phpincludes/navigation.php"; ?>
-  <h1>Hello Woild</h1>
-</body>
+<?php include_once("index.html"); ?>
