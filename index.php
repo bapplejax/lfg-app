795897c6616853163c36344112d03441407e7299
jacob bapple <jake.bapple@gmail.com>
2019-09-19 11:04:44 -0600

    trying to fix bugs

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php $thisPage="Home"; ?>
-<html lang="en">
-	<head>
-		<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
-		<meta name="title" content="Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
-		<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?>" />
-	  <meta charset="utf-8">
+<head>
+	<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
+	<meta name="title" content="Looking for Group | <?php if ($thisPage!="") echo " | $thisPage"; ?>" />
+	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
+  <meta charset="utf-8">
 
-	  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/styles.css">
 
-	</head>
+</head>
 
-	<body>
-		<?php require "/phpincludes/navigation.php"; ?>
-	  <h1>Hello Woild</h1>
-	</body>
-
-</html>
+<body>
+	<?php require "/phpincludes/navigation.php"; ?>
+  <h1>Hello Woild</h1>
+</body>
