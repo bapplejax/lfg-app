0fb2054f0b10ac885b2c08332bf85c9edb998ade
jacob bapple <jake.bapple@gmail.com>
2019-09-18 19:00:23 -0600

    testing again

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include_once("index.html"); ?>
+<html>
+	<header>
+		<title>Hello World</title>
+	</header>
+
+	<body>
+		<?php include_once("phpincludes/navigation.php"); ?>
+	  <h1>WHy World</h1>
+	</body>
+</html>
