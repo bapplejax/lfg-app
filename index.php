334104d725acf16266cc46a0eb03d7ab48e68eed
jacob bapple <jake.bapple@gmail.com>
2019-09-19 11:31:30 -0600

    abstracting head

--- index.php
+++ index.php
@@ -1,18 +1,9 @@
 <?php $thisPage="Home"; ?>
+<html>
+	<?php require_once('phpincludes/head.php'); ?>
 
-<head>
-	<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
-	<meta name="title" content="Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?>" />
-	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
-  <meta charset="utf-8">
-
-  <link rel="stylesheet" href="css/styles.css">
-
-</head>
-
-<body>
-	<h1>How about now</h1>
-	<?php echo "WHAasdfY"; ?>
-	<?php require_once('phpincludes/navigation.php'); ?>
-  <h1>Hello Woild</h1>
-</body>
+	<body>
+		<?php require_once('phpincludes/navigation.php'); ?>
+	  <h1>Hello Woild</h1>
+	</body>
+</html>
---
+++ phpincludes/head.php
@@ -0,0 +1,9 @@
+<?php ?>
+<head>
+	<title>Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?></title>
+	<meta name="title" content="Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?>" />
+	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
+  <meta charset="utf-8">
+
+  <link rel="stylesheet" href="css/styles.css">
+</head>
