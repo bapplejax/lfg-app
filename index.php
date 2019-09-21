ca74f2206654480a77f436350e9b498f5756bb98
jacob bapple <jake.bapple@gmail.com>
2019-09-21 14:20:19 -0600

    Refactor

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php $thisPage="Home"; ?>
 <html>
+
 	<?php require_once('phpincludes/head.php'); ?>
+
 	<body>
-		<?php require_once('phpincludes/header.php'); ?>
-		<div class="container--full lfg-banner__container">
+		<div class="lfg-header">
+			<?php require_once('phpincludes/header.php'); ?>
+		</div>
+
+		<div class="lfg-banner__container">
 			<?php require_once('phpincludes/banner.php'); ?>
 	  </div>
 	</body>
--- phpincludes/header.php
+++ phpincludes/header.php
@@ -1,25 +1,23 @@
 <?php ?>
-<div class="lfg-header">
-	<div class="lfg-header__top">
-		<img class="lfg-header__logo" src="https://www.fillmurray.com/200/75" />
+<div class="lfg-header__top">
+	<img class="lfg-header__logo" src="https://www.fillmurray.com/200/75" />
 
-		<p class="lfg-header__title">LOOKING<br/><span>for group</span></p>
+	<p class="lfg-header__title">LOOKING<br/><span>for group</span></p>
 
-		<div class="lfg-header__search-container">
-			<div class="lfg-header__search-icon">
-				<i class="icon icon--search"></i>
-			</div>
-			<input class="lfg-header__search-field" type="text" placeholder="Search" >
+	<div class="lfg-header__search-container">
+		<div class="lfg-header__search-icon">
+			<i class="icon icon--search"></i>
 		</div>
-
-		<div class="lfg-header__right">
-			<i class="icon icon--twitter"></i>
-			<!-- @TODO have an if statement for login status to change out icons/words -->
-			<a class="lfg-header__login" href="#"><i class="icon icon--user"></i>Log In/Register</a>
-		</div>
+		<input class="lfg-header__search-field" type="text" placeholder="Search" >
 	</div>
 
-	<div class="lfg-header__bottom">
-		<?php include_once('navigation.php'); ?>
+	<div class="lfg-header__right">
+		<i class="icon icon--twitter"></i>
+		<!-- @TODO have an if statement for login status to change out icons/words -->
+		<a class="lfg-header__login" href="#"><i class="icon icon--user"></i>Log In/Register</a>
 	</div>
-<div>
+</div>
+
+<div class="lfg-header__bottom">
+	<?php include_once('navigation.php'); ?>
+</div>
