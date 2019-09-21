cfef4f39b63af5dd7ae22ddb75e01b51e3d1b35f
jacob bapple <jake.bapple@gmail.com>
2019-09-21 14:57:26 -0600

    banner changes

--- phpincludes/banner.php
+++ phpincludes/banner.php
@@ -1,8 +1,8 @@
 <?php ?>
-<img src="https://www.fillmurray.com/2000/400" alt="Banner Image" />
+<img class="lfg-banner__image" src="https://www.fillmurray.com/2000/400" alt="Banner Image" />
 <!-- @TODO add in logic for logged in/premium/anonymous to change out text and cta -->
 <div class="lfg-banner__text-area">
-	<h1 class="lfg-banner__title">Welcome to Looking for Group</h1>
+  <h1 class="lfg-banner__title">Welcome to <br/><em>Looking for Group</em></h1>
 	<p class="lfg-banner__subtext">the site that helps you be social without socializing!</p>
 	<a class="lfg-banner__button button button--primary" href="/premium.php">Go Premium</a>
 </div>
