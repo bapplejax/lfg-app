78aeb23b3a743a6f4a62fbe54b2dc17df2e7c5f7
jacob bapple <jake.bapple@gmail.com>
2019-09-21 14:51:17 -0600

    refactoring css

--- phpincludes/navigation.php
+++ phpincludes/navigation.php
@@ -1,10 +1,10 @@
 <?php ?>
 <nav>
 	<ul class="lfg-nav">
-		<li<?php if ($thisPage=="Home") echo " class=\"active\""; ?>><a href="index.php">Home</a></li>
-		<li<?php if ($thisPage=="About") echo " class=\"active\""; ?>><a href="about.php">About</a></li>
-		<li<?php if ($thisPage=="Games") echo " class=\"active\""; ?>><a href="games.php">Games</a></li>
-		<li<?php if ($thisPage=="Go Premium") echo " class=\"active\""; ?>><a href="premium.php">Go Premium</a></li>
-		<li<?php if ($thisPage=="Contact") echo " class=\"active\""; ?>><a href="contact.php">Contact</a></li>
+		<li<?php if ($thisPage=="Home") echo " class=\"active\""; ?>><a href="/index.php">Home</a></li>
+		<li<?php if ($thisPage=="About") echo " class=\"active\""; ?>><a href="/about.php">About</a></li>
+		<li<?php if ($thisPage=="Games") echo " class=\"active\""; ?>><a href="/games.php">Games</a></li>
+		<li<?php if ($thisPage=="Go Premium") echo " class=\"active\""; ?>><a href="/premium.php">Go Premium</a></li>
+		<li<?php if ($thisPage=="Contact") echo " class=\"active\""; ?>><a href="/contact.php">Contact</a></li>
 	</ul>
 </nav>
