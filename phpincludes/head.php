2171a71a8ff983daa15ef2aeb8d701f77ca7aec9
jacob bapple <jake.bapple@gmail.com>
2019-09-24 09:26:20 -0600

    adding fonts

--- phpincludes/head.php
+++ phpincludes/head.php
@@ -4,6 +4,7 @@
 	<meta name="title" content="Looking for Group<?php if ($thisPage!="") echo " | $thisPage"; ?>" />
 	<meta name="keywords" content="<?php if ($thisPage!="")echo "$thisPage, "; ?> Looking for Group" />
   <meta charset="utf-8">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
 
-  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="/css/styles.css">
 </head>
