955d58ed8d3587e9684c2e72e599afc034b9b786
jacob bapple <jake.bapple@gmail.com>
2019-10-12 13:12:50 -0600

    Adding jQuery back.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // Get the modal
-var modal = document.getElementById("loginModal");
-console.log(modal);
+var $modal = document.getElementById("loginModal");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("lfg-header__login");
-console.log(btn);
+var $btn = document.getElementsByClassName("lfg-header__login");
 
 // Get the <span> element that closes the modal
-var close = document.getElementsByClassName("close")[0];
-console.log(close);
+var $close = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+$btn.onclick = function() {
+  $modal.addClass('modal-show');
+  console.log('clicked on button')
 };
 
 // When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-  modal.style.display = "none";
+$close.onclick = function() {
+  $modal.removeClass('modal-show');
+  console.log('clicked on close');
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == $modal) {
+    $modal.removeClass('modal-show');
+    console.log('click on window');
   }
 };
