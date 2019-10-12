0c1d819449fffb8fc2335ffbb933b6f7ef3853c6
jacob bapple <jake.bapple@gmail.com>
2019-10-12 13:39:12 -0600

    removing jquery objects.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 $(document).ready( function(){
   console.log('helloooo');
-  
+
   // Get the modal
-  var $modal = document.getElementById("loginModal");
+  var modal = document.getElementById("loginModal");
 
   // Get the button that opens the modal
-  var $btn = document.getElementsByClassName("lfg-header__login");
+  var btn = document.getElementsByClassName("lfg-header__login");
 
   // Get the <span> element that closes the modal
-  var $close = document.getElementsByClassName("close")[0];
+  var close = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal
-  $btn.onclick = function() {
-    $modal.addClass('modal-show');
+  btn.onclick = function() {
+    modal.addClass('modal-show');
     console.log('clicked on button')
   };
 
   // When the user clicks on <span> (x), close the modal
-  $close.onclick = function() {
-    $modal.removeClass('modal-show');
+  close.onclick = function() {
+    modal.removeClass('modal-show');
     console.log('clicked on close');
   };
 
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
-    if (event.target == $modal) {
-      $modal.removeClass('modal-show');
+    if (event.target == modal) {
+      modal.removeClass('modal-show');
       console.log('click on window');
     }
   };
