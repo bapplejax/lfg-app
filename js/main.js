b3107421d20bdad444697df075b0849194ad38c8
jacob bapple <jake.bapple@gmail.com>
2019-10-12 14:23:16 -0600

    something will work

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
 $(document).ready( function(){
 
   // Get the modal
-  const $modal = $("#loginModal");
-  console.log($modal);
+  const modal = $("#loginModal");
+  console.log(modal);
 
   // Get the button that opens the modal
-  const $btn = $(".lfg-header__login");
-  console.log($btn);
+  const btn = $(".lfg-header__login");
+  console.log(btn);
 
   // Get the <span> element that closes the modal
-  const $close = $(".close")[0];
-  console.log($close);
+  const close = $(".close");
+  console.log(close);
 
   // When the user clicks on the button, open the modal
-  $btn.click(function() {
+  btn.click(function() {
     console.log("clicked on button");
-    console.log($modal);
-    $modal.addClass('modal-show');
-    console.log(this + " " + $modal);
+    console.log(modal);
+    modal.addClass('modal-show');
+    console.log(this + " " + modal);
   });
 
   // When the user clicks on <span> (x), close the modal
-  $close.click(function() {
-    console.log($close);
-    $modal.removeClass('modal-show');
+  close.click(function() {
+    console.log(close);
+    modal.removeClass('modal-show');
   });
 
   // When the user clicks anywhere outside of the modal, close it
-  $(window).click(function(event) {
-    if (event.target == $modal) {
-      $modal.removeClass('modal-show');
+  window.click(function(event) {
+    if (event.target == modal) {
+      modal.removeClass('modal-show');
     }
   });
 });
