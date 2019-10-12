b5d26f658a284e6ceb327dddd3f7b11223697243
jacob bapple <jake.bapple@gmail.com>
2019-10-12 12:57:10 -0600

    jqeurying objects.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,28 @@
 // Get the modal
-var modal = document.getElementById("loginModal");
-console.log(modal);
+var $modal = $document.getElementById("loginModal");
+console.log($modal);
 
 // Get the button that opens the modal
-var btn = document.find(".lfg-header__login");
-console.log(btn);
+var $btn = $document.find(".lfg-header__login");
+console.log($btn);
 
 // Get the <span> element that closes the modal
-var close = document.find(".close")[0];
-console.log(close);
+var $close = $document.find(".close")[0];
+console.log($close);
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+$btn.onclick = function() {
+  $modal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-close.onclick = function() {
-  modal.style.display = "none";
+$close.onclick = function() {
+  $modal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+$window.onclick = function(event) {
+  if (event.target == $modal) {
+    $modal.style.display = "none";
   }
 };
