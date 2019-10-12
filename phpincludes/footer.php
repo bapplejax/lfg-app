2c7fe5559da49fc926df007805d60e46074aaef6
jacob bapple <jake.bapple@gmail.com>
2019-10-12 13:08:06 -0600

    Scripts at bottom of page so it actually works?

--- phpincludes/footer.php
+++ phpincludes/footer.php
@@ -4,3 +4,7 @@
   <p>Copyright 2019</p>
   <a href="/contact.php">Contact Us</a>
 </div>
+
+<!-- JQ and JS -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="/js/main.js"></script>
--- phpincludes/head.php
+++ phpincludes/head.php
@@ -11,8 +11,4 @@
 
   <!-- Styles -->
   <link rel="stylesheet" href="/css/styles.css">
-
-  <!-- JQ and JS -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="/js/main.js"></script>
 </head>
