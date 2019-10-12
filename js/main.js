b5c024941eb84e66fb4c7b44ffd75230bf58bef5
jacob bapple <jake.bapple@gmail.com>
2019-10-12 14:26:11 -0600

    sure

--- js/main.js
+++ js/main.js
@@ -2,32 +2,28 @@ $(document).ready( function(){
 
   // Get the modal
   const modal = $("#loginModal");
-  console.log(modal);
 
   // Get the button that opens the modal
   const btn = $(".lfg-header__login");
-  console.log(btn);
 
   // Get the <span> element that closes the modal
   const close = $(".close");
-  console.log(close);
+
+  // Get the wrapper element
+  const modalWindow = $(".modal__wrapper");
 
   // When the user clicks on the button, open the modal
   btn.click(function() {
-    console.log("clicked on button");
-    console.log(modal);
     modal.addClass('modal-show');
-    console.log(this + " " + modal);
   });
 
   // When the user clicks on <span> (x), close the modal
   close.click(function() {
-    console.log(close);
     modal.removeClass('modal-show');
   });
 
   // When the user clicks anywhere outside of the modal, close it
-  window.click(function(event) {
+  modalWindow.click(function(event) {
     if (event.target == modal) {
       modal.removeClass('modal-show');
     }
