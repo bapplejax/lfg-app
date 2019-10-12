646260bd4d09402a0b8d9465d92ad7fef36c8fdf
jacob bapple <jake.bapple@gmail.com>
2019-10-12 13:48:54 -0600

    again, with feeling.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,31 @@
 $(document).ready( function(){
 
-  const $myModal = $(document).find("#loginModal");
-  console.log($myModal);
-
   // Get the modal
-  var modal = document.getElementById("loginModal");
+  var $modal = $(document).find("#loginModal");
 
   // Get the button that opens the modal
-  var btn = document.getElementsByClassName("lfg-header__login");
+  var $btn = $(document).find(".lfg-header__login");
 
   // Get the <span> element that closes the modal
-  var close = document.getElementsByClassName("close")[0];
+  var $close = $(document).find(".close")[0];
 
   // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.addClass('modal-show');
+  $btn.on('click', function() {
+    $modal.addClass('modal-show');
     console.log('clicked on button')
-  };
+  });
 
   // When the user clicks on <span> (x), close the modal
-  close.onclick = function() {
-    modal.removeClass('modal-show');
+  $close.on('click', function() {
+    $modal.removeClass('modal-show');
     console.log('clicked on close');
-  };
+  });
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.removeClass('modal-show');
+  $(window).on('click', function(event) {
+    if (event.target == $modal) {
+      $modal.removeClass('modal-show');
       console.log('click on window');
     }
-  };
+  });
 });
