6c278f8208faccb43ef5e656ba412d1bf270472d
jacob bapple <jake.bapple@gmail.com>
2019-10-12 13:56:08 -0600

    Getting rid of console logs.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,28 @@
 $(document).ready( function(){
 
   // Get the modal
-  var $modal = $(document).find("#loginModal");
+  var $modal = $("#loginModal");
 
   // Get the button that opens the modal
-  var $btn = $(document).find(".lfg-header__login");
+  var $btn = $(".lfg-header__login");
 
   // Get the <span> element that closes the modal
-  var $close = $(document).find(".close")[0];
+  var $close = $(".close")[0];
 
   // When the user clicks on the button, open the modal
   $btn.click(function() {
     $modal.addClass('modal-show');
-    console.log('clicked on button')
   });
 
   // When the user clicks on <span> (x), close the modal
   $close.click(function() {
     $modal.removeClass('modal-show');
-    console.log('clicked on close');
   });
 
   // When the user clicks anywhere outside of the modal, close it
   $(window).click(function(event) {
     if (event.target == $modal) {
       $modal.removeClass('modal-show');
-      console.log('click on window');
     }
   });
 });
