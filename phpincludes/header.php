97ca1f3fe1da0e0fc379988ebf3d469c5fe3935b
jacob bapple <jake.bapple@autodesk.com>
2019-11-03 12:21:16 -0700

    Login form style changes

--- phpincludes/header.php
+++ phpincludes/header.php
@@ -45,13 +45,14 @@ if (isset($_SESSION['message'])) {
           <span class="close">&times;</span>
           
             <form class="login-form" action="../phpincludes/login_handler.php" method="POST">
-              <div>Login</div>
+              <h2>Login</h2>
               <label for="username">Username</label>
               <input type="text" name="username" required />
               <label for="password">Password</label>
               <input type="password" name="password" required />
-              <input type="submit">
+              <input class="button button--primary" type="submit">
             </form>
+            
             <a href="/create-user.php">New member? Create your profile!</a>
               
         </div>
