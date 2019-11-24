87fd922e659eec8bac0a516ec46f8980d1bdff7a
jacob bapple <jake.bapple@autodesk.com>
2019-11-24 12:08:31 -0700

    Switching back

--- phpincludes/lfg-cards.php
+++ phpincludes/lfg-cards.php
@@ -2,9 +2,9 @@
 session_start();
 require_once('api_handler.php');
 echo "Start"."<br/>";
-//$get_data = callAPI('POST', 'https://api-v3.igdb.com/games/', 'fields name,total_rating,platforms; sort total_rating desc; limit 20;');
+$get_data = callAPI('POST', 'https://api-v3.igdb.com/games/', 'fields name,total_rating,platforms; sort total_rating desc; limit 20;');
 
-$get_data = callAPI('POST', 'https://api-v3.igdb.com/genres/', 'fields *; where id = (8,9,11);');
+//$get_data = callAPI('POST', 'https://api-v3.igdb.com/genres/', 'fields *; where id = (8,9,11);');
 
 $response = json_decode($get_data, true);
 echo "Entire Response: ";
